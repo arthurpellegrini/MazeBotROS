54280eb9afd8a63c9535ac84979fc7d05051e9c6
Clément <clemtoon78700.bri@gmail.com>
2024-12-03 21:43:46 +0100

    Update : improve get_robot_position error and exceptions handling

--- soar_maze_escape/src/robot_controller.py
+++ soar_maze_escape/src/robot_controller.py
@@ -2,19 +2,31 @@ import rospy
 import numpy as np
 from geometry_msgs.msg import Twist
 import tf2_ros
+from scipy.spatial.transform import Rotation as R
 
 def get_robot_position():
-    tf_buffer = tf2_ros.Buffer()
-    tf_listener = tf2_ros.TransformListener(tf_buffer)
-    rospy.sleep(1)
-    transform = tf_buffer.lookup_transform('map', 'base_link', rospy.Time(0), rospy.Duration(2.0))
-    x = transform.transform.translation.x
-    y = transform.transform.translation.y
-    q = transform.transform.rotation
-    siny_cosp = 2.0 * (q.w * q.z + q.x * q.y)
-    cosy_cosp = 1.0 - 2.0 * (q.y**2 + q.z**2)
-    theta = np.arctan2(siny_cosp, cosy_cosp)
-    return x, y, theta
+    """Localise le robot dans le cadre 'map'. Renvoie (x, y, theta)."""
+    tfBuffer = tf2_ros.Buffer()
+    tf_listener = tf2_ros.TransformListener(tfBuffer)
+
+    while True:
+        try:
+            trans = tfBuffer.lookup_transform('map', 'base_link', rospy.Time(0), rospy.Duration(1.0))
+            break
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+            rospy.logwarn("Robot localisation took longer than 1 sec")
+            continue
+
+    theta = R.from_quat([
+        trans.transform.rotation.x,
+        trans.transform.rotation.y,
+        trans.transform.rotation.z,
+        trans.transform.rotation.w]).as_euler("xyz")[2]
+
+    rospy.loginfo(f"Position du robot : x={trans.transform.translation.x}, y={trans.transform.translation.y}, theta={theta}")
+    return trans.transform.translation.x, trans.transform.translation.y, theta
+
+
 
 def world_to_grid(world_x, world_y, resolution, origin):
     """Converts world coordinates to grid coordinates."""
