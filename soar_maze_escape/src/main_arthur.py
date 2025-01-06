be366b77ce518a50988b12a1216028f278253e5d
Arthur PELLEGRINI <arthur.pellegrini@outlook.fr>
2025-01-06 15:01:34 +0000

    WIP: Adding rotations to global path

---
+++ soar_maze_escape/src/main_arthur.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+
+import rospy
+from map_utils import getMap, transformMap
+from graph_utils import buildGraph, findExits, a_star_search, findClosestNode, Node
+from visualization_utils import plotMap, plotGraph, plotNodePositionGraph
+from robot_controller import localiseRobot, goToNode
+import numpy as np
+
+def main():
+    rospy.init_node("moro_maze_navigation")
+    recMap = getMap()
+    freepoints, wallpoints = transformMap(recMap)
+    # plotMap(freepoints, wallpoints)
+
+    # Convert map to 2D grid
+    grid = np.array(recMap.data).reshape((recMap.info.height, recMap.info.width))
+
+    # Build graph
+    nodes, edges = buildGraph(grid)
+
+    # Add exits to the graph
+    nodes, find_exits = findExits(grid, nodes)
+    edges += find_exits
+
+    print("Nodes:", len(nodes), "- Edges:", len(edges))
+    for edge in edges:
+        print(edge)
+
+    robot_pos = localiseRobot()
+    print("robot pose",robot_pos)
+    # plotGraph(recMap, edges, wallpoints, robot_pos)
+    plotNodePositionGraph(recMap, nodes, edges, wallpoints, robot_pos)
+
+    # A* search
+    start_node = findClosestNode(robot_pos, edges, recMap)
+
+    for pos in nodes:
+        if start_node.position == nodes[pos].position:
+            start_exits = True
+
+    if start_exits:
+        paths = [a_star_search(grid, nodes, edges, start_node, exit.child) for exit in find_exits]
+
+        print("Paths found:", [[step.position for step in path] for path in paths])
+
+        # Get the path with the lower number of steps
+        path = min(paths, key=lambda x: len(x)) if paths else None
+
+        print("Path found:", [step.position for step in path])
+
+        # Add rotation for each step
+        global_path = []
+
+        for i in range(1, len(path)):
+            current_node = path[i - 1]
+            next_node = path[i]
+            print(f"From {current_node.position} to {next_node.position}")
+            # Compute the rotation
+            rotation = np.arctan2(next_node.position[0] - current_node.position[0], next_node.position[1] - current_node.position[1])
+            global_path.append((next_node.position[1], next_node.position[0], rotation))
+            # robot_pos = goToNode(robot_pos, next_node, recMap)
+        
+        print("Global path:", global_path)
+    else:
+        print("Start or goal node is invalid.")
+
+
+    # exemple node ([y, x])
+    # new_robot_pos = [0, 0, 0]
+    # for edge in edges:
+    #     print(edge.parent.position)
+    #     if(edge.parent.position == (22, 10)):
+    #             new_robot_pos = goToNode(robot_pos, edge.parent, recMap)
+    #             print("new_robot_pos", new_robot_pos)
+    #     if(new_robot_pos != [0,0,0] and edge.parent.position == (22, 22)):
+    #         goToNode(new_robot_pos, edge.parent, recMap)
+
+if __name__ == "__main__":
+    main()
